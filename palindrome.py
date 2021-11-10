f9f472043090aaf67fa83bb8c2e838b27d040147
Fernando Callasaca <fernandocallasaca@outlook.com>
2021-11-10 10:34:21 -0500

    palindrome using typing methos

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,13 @@
-print('hello world')
+# verify if it is a palindrome word
+def is_palindrom(word: str) -> bool:
+   # first replace 
+   word = word.replace(" ", '').lower()
+   return(word == word[::-1])
+
+def run():
+   # enter a word
+   word = input('Enter a word to veryfy if its palindrome: ')
+   print(is_palindrom(word))
+
+if __name__ == '__main__':
+   run()
